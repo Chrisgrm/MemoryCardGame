e5a412e8e348209f20aa8ea2181ca645ccb180d5
David-bx2 <121781056+David-bx2@users.noreply.github.com>
2023-05-03 12:51:43 -0600

    Update MenuPausa.cs
    añadido script menu pausa

--- Assets/Scripts/MenuPausa.cs
+++ Assets/Scripts/MenuPausa.cs
@@ -1,11 +1,36 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using TMPro;
+using UnityEngine.UI;
 
 public class MenuPausa : MonoBehaviour
 {
+    public TextMeshProUGUI scoreText;
+    public TextMeshProUGUI gameOverText;
+    public GameObject titleScreen;
+    private Button pausabutton;
+    public GameObject panelPausa;
+    public GameObject canvasPrincipal;
+
+    void Start()
+    {
+
+    }
     public void Pausa()
     {
         Time.timeScale = 0f;
+        panelPausa.SetActive(true);
+    }
+
+    public void back()
+    {
+        panelPausa.SetActive(false);
+    }
+
+    public void MenuPrincipal()
+    {
+        titleScreen.SetActive(true);
+        canvasPrincipal.SetActive(false);
     }
 }
