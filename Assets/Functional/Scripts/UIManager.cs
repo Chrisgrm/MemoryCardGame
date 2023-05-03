fa7b8f513c41296148d27b60e9c12074f415d75d
Chrisgrm <chrisgrm@hotmail.com>
2023-05-03 13:49:02 -0500

    Proyecto semi organizado, cartas al 100%, UI 50%

--- Assets/Functional/Scripts/UIManager.cs
+++ Assets/Functional/Scripts/UIManager.cs
@@ -1,18 +1,30 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using TMPro;
+using UnityEngine.UI;
 
 public class UIManager : MonoBehaviour
 {
-    // Start is called before the first frame update
+    public GameObject panelVictoria;
+    public GameObject panelPausa;
+    private Button botonPausa;
+    public TMP_Text movimientosContador;
     void Start()
+    {
+        botonPausa = GetComponent<Button>();
+    }
+
+    
+    void Update()
     {
         
     }
 
-    // Update is called once per frame
-    void Update()
+    public void BotonPausaFunc()
     {
-        
+        print("funciono");
+        botonPausa.gameObject.SetActive(false);
+        panelPausa.SetActive(true);
     }
 }
