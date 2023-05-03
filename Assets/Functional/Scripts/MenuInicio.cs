97dca92e6c9181310f999afba8b18c80bceea41f
David-bx2 <121781056+David-bx2@users.noreply.github.com>
2023-05-03 13:34:16 -0600

    Añadido el codigo del menu inicio
    se añadio boton back en la pagina de credits, y se hizo el codigo para el manejo del menu de inicio

---
+++ Assets/Functional/Scripts/MenuInicio.cs
@@ -0,0 +1,30 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using TMPro;
+using UnityEngine.SceneManagement;
+
+public class MenuInicio : MonoBehaviour
+{
+    public GameObject creditsScreen;
+    public GameObject menuInicio;
+
+    public void StartGame()
+    {
+        //SceneManager.LoadScene(AnimationScene2);
+        menuInicio.SetActive(false);
+    }
+
+    public void Credits()
+    {
+        creditsScreen.SetActive(true);
+        menuInicio.SetActive(false);
+    }
+
+    public void creditsBack()
+    {
+        creditsScreen.SetActive(false);
+        menuInicio.SetActive(true);
+    }
+}
